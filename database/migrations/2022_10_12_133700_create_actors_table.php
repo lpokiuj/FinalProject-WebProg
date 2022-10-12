da1075bb92e997fdb17ef371d954e9098c649625
lpokiuj <michael.sanjaya86@gmail.com>
2022-10-12 22:44:31 +0700

    Actor CRUD

---
+++ database/migrations/2022_10_12_133700_create_actors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('gender', ['Male', 'Female']);
+            $table->string('biography');
+            $table->date('DOB');
+            $table->string('POB');
+            $table->string('image');
+            $table->double('popularity', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actors');
+    }
+}
