3d1e79ee4258e488ea86c2f8d68c1d0d487be719
emmoki <emmanuelnuswantoro@gmail.com>
2022-10-17 13:13:35 +0700

    routes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,19 +5,20 @@
 @section('title', 'login')
 
 @section('content')
-    <div class="body">
-        <div class="body-headlines">
-            <div class="body-headlines-1">
-                Hello, Welcome back to
-            </div>
-            <div class="body-headlines-2">
-                <div class="title">
-                    <div class="title-1">Movie</div>
-                    <div class="title-2">List</div>
-                </div>
+<div class="body">
+    <div class="body-headlines">
+        <div class="body-headlines-1">
+            Hello, Welcome back to
+        </div>
+        <div class="body-headlines-2">
+            <div class="title">
+                <div class="title-1">Movie</div>
+                <div class="title-2">List</div>
             </div>
         </div>
-        <div class="filling">
+    </div>
+    <div class="filling">
+        <form action="">
             <div class="login">
                 <label for="email" class="email">E-mail</label>
                 <input type="text" id="email" name="email" placeholder="Enter your email">
@@ -34,115 +35,115 @@
                 <label>Login</label>
                 <img src="/assets/login/Arrow Right.svg" alt="" srcset="">
             </a>
-            <div class="dont-have-account">
-                <label>Don't have an account?</label>
-                <a href="#">Register Now!</a>
-            </div>
+        </form>
+        <div class="dont-have-account">
+            <label>Don't have an account?</label>
+            <a href="#">Register Now!</a>
         </div>
     </div>
-    <style>
+</div>
+<style>
+    .body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #121318;
+        flex-direction: column;
+        height: 700px;
+    }
 
-        .body {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: #121318;
-            flex-direction: column;
-            height: 700px;
-        }
+    .body-headlines {
+        display: flex;
+    }
 
-        .body-headlines {
-            display: flex;
-        }
+    .body-headlines-1 {
+        font-family: sans-serif;
+        font-weight: 600;
+        font-size: xx-large;
+        color: white;
 
-        .body-headlines-1 {
-            font-family: sans-serif;
-            font-weight: 600;
-            font-size: xx-large;
-            color: white;
+    }
 
-        }
+    .body-headlines-2 {
+        margin-left: -70px;
+    }
 
-        .body-headlines-2 {
-            margin-left: -70px;
-        }
+    .filling {
+        margin-top: 2rem;
+    }
 
-        .filling {
-            margin-top: 2rem;
-        }
+    .login {
+        background-color: #1F1F1F;
+        padding: 0.5rem 2rem 0.5rem 2rem;
+        border-radius: 10px;
+        margin-top: 1rem;
+    }
 
-        .login {
-            background-color: #1F1F1F;
-            padding: 0.5rem 2rem 0.5rem 2rem;
-            border-radius: 10px;
-            margin-top: 1rem;
-        }
+    .login label {
+        color: white;
+        margin-right: 10rem;
+        font-size: large;
+        font-family: sans-serif;
+    }
 
-        .login label {
-            color: white;
-            margin-right: 10rem;
-            font-size: large;
-            font-family: sans-serif;
-        }
+    .login input {
+        text-decoration: none;
+        font-size: large;
+        background-color: #1F1F1F;
+        border-style: none;
+        color: white;
+        width: 300px;
+        padding: 0.5rem;
+    }
 
-        .login input {
-            text-decoration: none;
-            font-size: large;
-            background-color: #1F1F1F;
-            border-style: none;
-            color: white;
-            width: 300px;
-            padding: 0.5rem;
-        }
+    .password {
+        margin-left: -1.5rem;
+    }
 
-        .password {
-            margin-left: -1.5rem;
-        }
+    .remember-me {
+        margin-top: 1rem;
+        color: white;
+        font-family: sans-serif;
+    }
 
-        .remember-me {
-            margin-top: 1rem;
-            color: white;
-            font-family: sans-serif;
-        }
+    .remember-me label {
+        margin-left: 0.5rem;
+    }
 
-        .remember-me label {
-            margin-left: 0.5rem;
-        }
+    .login-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #E50913;
+        margin-top: 1rem;
+        border-radius: 5px;
+        padding: 0.5rem 0;
+        text-decoration: none;
+    }
 
-        .login-button {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: #E50913;
-            margin-top: 1rem;
-            border-radius: 5px;
-            padding: 0.5rem 0;
-            text-decoration: none;
-        }
+    .login-button label {
+        font-size: large;
+        font-family: sans-serif;
+        text-decoration: none;
+        color: white;
+    }
 
-        .login-button label {
-            font-size: large;
-            font-family: sans-serif;
-            text-decoration: none;
-            color: white;
-        }
+    .login-button img {
+        filter: invert(100%) sepia(93%) saturate(27%) hue-rotate(233deg) brightness(107%) contrast(100%);
+    }
 
-        .login-button img {
-            filter: invert(100%) sepia(93%) saturate(27%) hue-rotate(233deg) brightness(107%) contrast(100%);
-        }
+    .dont-have-account {
+        display: flex;
+        justify-content: center;
+        margin-top: 10px;
+        color: white;
+        font-weight: 550;
+        font-family: sans-serif;
+    }
 
-        .dont-have-account {
-            display: flex;
-            justify-content: center;
-            margin-top: 10px;
-            color: white;
-            font-weight: 550;
-            font-family: sans-serif;
-        }
-
-        .dont-have-account a {
-            color: #E50913;
-            text-decoration: none;
-        }
-    </style>
+    .dont-have-account a {
+        color: #E50913;
+        text-decoration: none;
+    }
+</style>
 @endsection
